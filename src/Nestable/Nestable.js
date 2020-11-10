07f03069f278e741a6a9798533a91baf3ce5fa41
badie21 <m.badiehneshin@barpin.net>
2020-11-10 15:01:44 +0330

    change default

--- src/Nestable/Nestable.js
+++ src/Nestable/Nestable.js
@@ -62,7 +62,7 @@ class Nestable extends Component {
 		collapsed: false,
 		group: Math.random().toString(36).slice(2),
 		childrenProp: 'children',
-		rowDirection: 'rtl',
+		rowDirection: 'ltr',
 		renderItem: ({ item }) => item.toString(),
 		onChange: () => {},
 		confirmChange: () => true,
